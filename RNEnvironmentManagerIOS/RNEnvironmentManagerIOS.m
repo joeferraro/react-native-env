5354ee586ef357aeb370de25702803fe4fcde705
Mark Vayngrib <mark@tradle.io>
2016-01-05 19:18:24 -0500

    synchronous access to env

--- RNEnvironmentManagerIOS/RNEnvironmentManagerIOS.m
+++ RNEnvironmentManagerIOS/RNEnvironmentManagerIOS.m
@@ -5,28 +5,9 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(get:(NSString *)name callback:(RCTResponseSenderBlock)callback) {
-    @try {
-        NSDictionary *env = [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"environment" ofType:@"plist"]];
-        if ([env objectForKey:name]) {
-            callback(@[[NSNull null], env[name]]);
-        } else {
-            callback(@[[NSNull null], [NSNull null]]);
-        }
-    }
-    @catch (NSException *exception) {
-        callback(@[exception.reason, [NSNull null]]);
-    }
-}
-
-RCT_EXPORT_METHOD(getAll:(RCTResponseSenderBlock)callback) {
-    @try {
-        NSDictionary *env = [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"environment" ofType:@"plist"]];
-        callback(@[[NSNull null], env]);
-    }
-    @catch (NSException *exception) {
-        callback(@[exception.reason, [NSNull null]]);
-    }
+- (NSDictionary *)constantsToExport
+{
+    return [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"environment" ofType:@"plist"]];
 }
 
 @end
