19361827c84ad4a3a9d761fdadd8d65f0e814856
Mark Vayngrib <mark@tradle.io>
2016-01-07 23:45:49 -0500

    err handling

--- RNEnvironmentManagerIOS/RNEnvironmentManagerIOS.m
+++ RNEnvironmentManagerIOS/RNEnvironmentManagerIOS.m
@@ -11,13 +11,22 @@ RCT_EXPORT_MODULE()
   if (dict) return dict;
 
   NSData *data = [NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"environment" ofType:@"json"]];
+  if (!data) {
+    return [RNEnvironmentManagerIOS getEnvNotFoundError];
+  }
+
   NSError *error = nil;
   id json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
   if (error) {
-    return @{ @"error": @"environment.json not found. Did you forget to Add Files to your project?" };
+    return [RNEnvironmentManagerIOS getEnvNotFoundError];
   } else {
     return json;
   }
+}
+
++ (NSDictionary *) getEnvNotFoundError
+{
+  return @{ @"error": @"neither environment.plist nor environment.json were found. Did you forget to Add Files to your project?" };
 }
 
 @end
