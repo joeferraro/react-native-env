d8ad9243d369480f1a2d9633d0418fc6aeaaf392
Alex McLeod <alex@mcleod.io>
2016-02-24 13:25:45 -0800

    Fixed reference to RNEnvironmentManager

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ module.exports = {
   },
 
   getSync(name) {
-    if (!RNEnvironmentManager[name]) console.error('Could not get env var', name, '. Has it been defined in environment.plist?');
-    return RNEnvironmentManager[name];
+    if (!RNEnvironmentManagerIOS[name]) console.error('Could not get env var', name, '. Has it been defined in environment.plist?');
+    return RNEnvironmentManagerIOS[name];
   }
 }
